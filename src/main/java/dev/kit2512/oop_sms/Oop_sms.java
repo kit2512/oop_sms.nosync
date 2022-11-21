047526b96a7056b77a2f6964d0f706ac56c99b6e
Hung Nguyen <dannamdinh49@gmail.com>
2022-11-21 15:44:47 +0700

    Initial commit

---
+++ src/main/java/dev/kit2512/oop_sms/Oop_sms.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package dev.kit2512.oop_sms;
+
+/**
+ *
+ * @author macpro13
+ */
+public class Oop_sms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
